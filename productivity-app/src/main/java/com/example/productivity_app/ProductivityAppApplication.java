91986ab8a87202aefa95efb09adc467543ecd917
Olivia Hughes <oliviahughes1103@outlook.com>
2026-08-30 08:35:39 +0100

    first commit

---
+++ productivity-app/src/main/java/com/example/productivity_app/ProductivityAppApplication.java
@@ -0,0 +1,15 @@
+// Auto-generated code provided by the spring initialiser.
+
+package com.example.productivity_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityAppApplication.class, args);
+	}
+
+}
